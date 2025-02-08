2026290444e78b3acda5d286c7c4270a362276f9
feyrouzdjebbara <feyrouzdjebbara2000@gmail.com>
2025-02-08 10:27:43 -1100

    Merged local changes with remote changes

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: "I'm DJEBBARA Feyrouz, a versatile Software Engineer and Graphic Designer. With a passion for problem-solving and a love for JavaScript, I thrive in crafting innovative web solutions. Eager to explore new technologies and contribute to impactful projects. Open to exciting job opportunities that match my skills and interests.",
   email: 'feyrouzdjebbara2000@gmail.com',
   phone: '+213794417755',
-  address: 'Si Mustapha Boumerdes Algeria',
+  address: 'Sidi abdella Algiers Algeria',
   github: 'https://github.com/feyrouzdjebbara',
   facebook: '',
   linkedIn: 'https://www.linkedin.com/in/feyrouz-djebbara-a4969319b/',
